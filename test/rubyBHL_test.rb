17977e2c98ef0353f756c9d2eab880a87a5256b6
Matt <diapriid@gmail.com>
2009-10-19 17:56:04 -0400

    initial import

---
+++ test/rubyBHL_test.rb
@@ -0,0 +1,36 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/rubyBHL'))
+
+class RubyBHLTest < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def test_foo_is_not_a_legal_param
+    assert_raise(RubyBHL::RbhlError) {RubyBHL::Rbhl.new(:params => {:foo => "BAR!"}) }   
+  end
+  
+  def test_search_url_for_default_url_is_properly_formed
+    @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR!", :volume => "2"})
+    assert_equal "http://www.biodiversitylibrary.org/openurl?title=BAR!&volume=2&format=json", @bhl.search_url 
+  end
+  
+  def test_search_url_convert_space_to_plus
+    @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR FOO"})
+    assert_equal "http://www.biodiversitylibrary.org/openurl?title=BAR+FOO&format=json", @bhl.search_url 
+  end
+  
+  def test_basic_response_is_returned
+   # this is a crummy test, we need a reference to the status codes.
+   @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR FOO"}) # should match nothing, I hope
+   assert @bhl.json_data["Status"] == 1
+   assert @bhl.json_data["citations"].size == 0
+  end
+  
+  def test_response_with_citations_is_returned
+   # this is a crummy test, we need a reference to the status codes.
+   @bhl = RubyBHL::Rbhl.new(:params => {:title => "Manual of North American Diptera"}) # should match nothing, I hope
+   assert @bhl.json_data["citations"].size > 0
+  end
+
+end
