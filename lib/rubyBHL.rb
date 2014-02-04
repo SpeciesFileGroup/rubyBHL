ad83a8bdbcf6c6fbeec25d7415d6a3094669c903
Matt Yoder <diapriid@gmail.com>
2014-02-04 16:33:12 -0600

    Misc crud that has been uncommitted for many years.

--- lib/rubyBHL.rb
+++ lib/rubyBHL.rb
@@ -1,119 +1,30 @@
 module RubyBHL
-  
- # Some quick hacks, only configured for json right now.
 
- require 'net/http'
- # require 'json'
- require 'json/add/rails'
+require 'net/http'
+require 'json/add/rails'
+# require 'json'
 
- class RbhlError < StandardError
- end
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/Request'))
 
- class Rbhl
-   
- # some notes   
- #http://www.biodiversitylibrary.org/openurl?url_ver=Z39.88-2004&ctx_ver=Z39.88-2004
- #rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook
- #rft.btitle
- #rft.jtitle
- #rft.au
- #rft.aufirst
- #rft.aulast
- #rft.publisher
- #rft.pub
- #rft.place
- #rft.date
- #rft.issn
- #rft.isbn
- # rft.coden
- # rft.stitle
- # rft.volume
- # rft.issue
- # rft.spage
- # rft_id=info:oclcnum/XXXX
- # rft_id=info:lccn/XXXX
- # rft_id=http://www.biodiversitylibrary.org/bibliography/XXXX
- # rft_id=http://www.biodiversitylibrary.org/page/XXXX
-  
-  # "constants" (defaults really)
-  SEARCH_URL =  'http://www.biodiversitylibrary.org/openurl?' 
-  FORMAT = 'json'
-  #  METHOD = '' # openURL0.1, openURL1.0 
-
-  PARAMETERS = [
-                  :title,
-                  :au,
-                  :aufirst,
-                  :aulast,
-                  :publisher,
-                  :date,
-                  :issn,
-                  :isbn,
-                  :coden,
-                  :stitle,
-                  :volume,
-                  :issue,
-                  :spage]
-  
-  # created from response
-  attr_reader(:json_data)
-  attr_reader(:citations)
-  attr :search_url
-
- def initialize(options = {}) 
-   @opt = {
-    #  :method => METHOD,
-      :format => FORMAT,
-      :params => {}
-   }.merge!(options)
-   
-   # check for legal parameters
-   @opt[:params].keys.each do |p|
-      raise RbhlError, "#{p} is not a valid parameter" if !PARAMETERS.include?(p)
-   end 
-     
-   @json_data = {}
-     
-   @search_url = SEARCH_URL + 
-       @opt[:params].keys.sort{|a,b| a.to_s <=> b.to_s}.collect{|k| "#{k}=#{@opt[:params][k].gsub(/\s/, "+")}"}.join("&") +
-      '&format=' + @opt[:format]       
-       
-   @json_data = JSON.parse(Net::HTTP.get_response(URI.parse(@search_url)).body)
-   @citations = @json_data['citations'] if @json_data['Status'] == 1 # a simpler reference
-   
-   true
- end
-
- # this works on a redirect
- def OCR_text(citation_index)
-   return nil if !citation_index
-   fetch(OCR_url(citation_index))
- end
-
- # Since the API doesn't return a link to the OCR we screen scrape it the URL
- def OCR_url(citation_index)
-   Net::HTTP.get_response(URI.parse(@citations[citation_index]["Url"])).body =~ /http:\/\/.*?\.txt/
-   return $& # the matched results
- end
-
- protected
- 
- # from the ruby doc
- def fetch(uri_string, limit = 10)
-   return nil if !uri_string
-   limit = 10 # Justin Case we get in some redirect loop
-   raise RbhlError, 'HTTP redirect too deep' if limit == 0 # should tweak
-   response = Net::HTTP.get_response(URI.parse(uri_string))
-   case response
-   when Net::HTTPSuccess then response.body
-   when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-   else
-     response.error!
-    end
-  end
- 
-
+class RbhlError < StandardError
 end
 
+class Rbhl
+  attr_accessor :opt
+  
+  def initialize(options = {}) 
+    # initialize once use many?
+    @opt = {
+      :openurl_version => '0.1',
+    }.merge!(options)  
+ end
+
+ def openURLRequest(options = {})
+   opt = {:params => {}}.merge(@opt).merge(options)
+
+   Request.new(opt)
+ end
+
+end
 
 end
--- test/rubyBHL_test.rb
+++ test/rubyBHL_test.rb
@@ -1,36 +1,47 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/rubyBHL'))
 
+include RubyBHL
+
 class RubyBHLTest < Test::Unit::TestCase
 
   def setup
   end
 
+  def test_vanilla_init
+    assert @bhl = Rbhl.new()
+  end
+
+  def test_vanilla_request
+    @bhl = Rbhl.new
+    assert @bhl.openURLRequest()
+  end
+
   def test_foo_is_not_a_legal_param
     assert_raise(RubyBHL::RbhlError) {RubyBHL::Rbhl.new(:params => {:foo => "BAR!"}) }   
   end
-  
+
   def test_search_url_for_default_url_is_properly_formed
     @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR!", :volume => "2"})
     assert_equal "http://www.biodiversitylibrary.org/openurl?title=BAR!&volume=2&format=json", @bhl.search_url 
   end
-  
+
   def test_search_url_convert_space_to_plus
     @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR FOO"})
     assert_equal "http://www.biodiversitylibrary.org/openurl?title=BAR+FOO&format=json", @bhl.search_url 
   end
-  
+
   def test_basic_response_is_returned
-   # this is a crummy test, we need a reference to the status codes.
-   @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR FOO"}) # should match nothing, I hope
-   assert @bhl.json_data["Status"] == 1
-   assert @bhl.json_data["citations"].size == 0
+    # this is a crummy test, we need a reference to the status codes.
+    @bhl = RubyBHL::Rbhl.new(:params => {:title => "BAR FOO"}) # should match nothing, I hope
+    assert @bhl.json_data["Status"] == 1
+    assert @bhl.json_data["citations"].size == 0
   end
-  
+
   def test_response_with_citations_is_returned
-   # this is a crummy test, we need a reference to the status codes.
-   @bhl = RubyBHL::Rbhl.new(:params => {:title => "Manual of North American Diptera"}) 
-   assert @bhl.json_data["citations"].size > 0
+    # this is a crummy test, we need a reference to the status codes.
+    @bhl = RubyBHL::Rbhl.new(:params => {:title => "Manual of North American Diptera"}) 
+    assert @bhl.json_data["citations"].size > 0
   end
 
   def test_OCR_text_scraping
@@ -38,5 +49,6 @@ class RubyBHLTest < Test::Unit::TestCase
     assert @bhl.OCR_text(0).size > 0
     assert @bhl.OCR_text(0) =~ /Diptera/ 
   end
+
 
 end
